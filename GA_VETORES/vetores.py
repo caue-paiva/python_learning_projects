bf1dccc11fae5da167e6eb6eff987437a955aec3
caue paiva <cauepaivalira@outlook.com>
2023-12-06 22:57:41 -0300

    mais metodos nos vetores

--- GA_VETORES/vetores.py
+++ GA_VETORES/vetores.py
@@ -1,12 +1,15 @@
 from math import sqrt, acos
 
-class vetores():
-   lista_coord: list = []
+
+
+class vetor():
+   lista_coord: list  #templates das variaveis individuais de cada classe
+   dimensoes: int 
+   RAD_PARA_GRAUS: float  = 57.29 # const de conversao usado por todos elementos da classe
 
    def __init__(self, dimensoes:int):
       self.dimensoes = dimensoes
-      for i in range(dimensoes):
-         self.lista_coord[i] = 0
+      self.lista_coord = [0] * dimensoes #inicializa a lista com 0 nas posicoes até dimensoes
    
    def __getitem__(self, index:int):
         if index <= self.dimensoes:
@@ -21,56 +24,64 @@ class vetores():
            print("o vetor nao tem essa dimensao")
        
    def __add__(self, outro_vetor: object)->object:
-      if isinstance(outro_vetor,vetores):     
+      if isinstance(outro_vetor,vetor):     
          if self.dimensoes != outro_vetor.dimensoes:
-            print("soma vetorial requer 2 vetores de mesmo tamanho")
+            print("soma vetorial requer 2 vetor de mesmo tamanho")
          else:
-            novo_vetor = vetores(self.dimensoes)
+            novo_vetor = vetor(self.dimensoes)
             for i in range(self.dimensoes):
-               novo_vetor[i] = self[i] + outro_vetor[i]
+               novo_vetor[i] = ( (self[i]) + (outro_vetor[i]) )
             return novo_vetor
          
       else:
-         print("soma vetorial requer 2 vetores da mesma classe")
+         print("soma vetorial requer 2 vetor da mesma classe")
 
    def __sub__(self, outro_vetor: object)->object:
-      if isinstance(outro_vetor,vetores):     
+      if isinstance(outro_vetor,vetor):     
          if self.dimensoes != outro_vetor.dimensoes:
-            print("subtracao vetorial requer 2 vetores de mesmo tamanho")
+            print("subtracao vetorial requer 2 vetor de mesmo tamanho")
          else:
-            novo_vetor = vetores(self.dimensoes)
+            novo_vetor = vetor(self.dimensoes)
             for i in range(self.dimensoes):
                novo_vetor[i] = self[i] - outro_vetor[i]
             return novo_vetor       
       else:
-         print("subtracao vetorial requer 2 vetores da mesma classe")
+         print("subtracao vetorial requer 2 vetor da mesma classe")
 
-   def __mod__(self, outro_vetor: object)->float:  #produto escalar: vetor1 % vetor2
-      if isinstance(outro_vetor,vetores):     
+   def __mul__(self, outro_vetor: object)->float:  #produto escalar: vetor1 * vetor2
+      if isinstance(outro_vetor,vetor):     
          if self.dimensoes != outro_vetor.dimensoes:
-            print("produto escalar requer 2 vetores de mesmo tamanho")
+            print("produto escalar requer 2 vetor de mesmo tamanho")
          else:
             prod_escalar: float = 0
             for i in range(self.dimensoes):
                prod_escalar += (self[i] * outro_vetor[i])
             return  prod_escalar
       else:
-         print("produto escalar requer 2 vetores da mesma classe")
+         print("produto escalar requer 2 vetor da mesma classe")
 
-   def coordenadas(self)->list:
+   def seta_valores(self, lista_coord: list): #enche um vetor com uma lista de coordenadas (float ou int)
+      if len(lista_coord) != self.dimensoes:
+          print("o vetor nao tem essa dimensao")
+          return 
+      else:
+         for i in range(self.dimensoes):
+            self.lista_coord[i] = lista_coord[i]
+
+   def valores(self)->list:
       return self.lista_coord
    
    def dimensao(self)-> int:
       return self.dimensoes
    
-   def modulo(self)->float:
-      soma: int 
+   def modulo(self)->float: #modulo do vetor
+      soma: int = 0
       for i in range(self.dimensoes):
          soma += pow(self.lista_coord[i],2)
       return sqrt(soma)
    
-   def angulo(self, outro_vetor: object)->float:  #retorna o angulo entre 2 vetores  em radiano 
-      if isinstance(outro_vetor,vetores):     
+   def angulo_rad(self, outro_vetor: object)->float:  #retorna o angulo entre 2 vetores  em radiano 
+      if isinstance(outro_vetor,vetor):     
          if self.dimensoes != outro_vetor.dimensoes:
             print("calcular angulo requer 2 vetores de mesmo tamanho")
          else:
@@ -79,12 +90,39 @@ class vetores():
             if modulo1 == 0 or modulo2 ==0:
                print("nao se pode calcular angulo de vetores nulos")
                return
-            prod_escalar: float = self % outro_vetor
+            prod_escalar: float = self * outro_vetor
             return acos(prod_escalar/(modulo1*modulo2))       
       else:
          print("calcular angulo requer 2 vetores da mesma classe")
    
+   def angulo_grau(self,outro_vetor:object)->float:
+      return self.RAD_PARA_GRAUS * self.angulo_rad(outro_vetor)
+   
+   def normalizar(self)-> object: #retorna um vetor paralelo ao atual porém com norma/modulo = 1
+        novo_vetor = vetor(self.dimensoes)
+        modulo: float = self.modulo()
+        if modulo == 0:
+           print("nao e possivel normalizar um vetor com modulo zero")
+           return None
+        
+        for i in range(self.dimensoes):
+           novo_vetor[i] = self[i]/modulo
 
+        return novo_vetor
+
+   
+vetor1 = vetor(2)
+
+vetor2 = vetor(2)
+
+vetor1.seta_valores([1,2])
+
+vetor2.seta_valores([2,-1])
+
+vetor3 = vetor1.normalizar()
+
+#print(prod_escalar)
+print(vetor3.valores())
    
 
          
